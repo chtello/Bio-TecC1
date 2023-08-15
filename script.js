9c6dadd3b6357577e988d298642b822a73f437ee
chtelo <chtello@escuelasproa.edu.ar>
2023-08-14 23:00:32 -0300

    Base lista

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-
 const express = require('express');
-const mongoose = require (mongoose);
+const morgan = require ("morgan");
+const bodyparser = require('bodyparser');
+const path = require ("path");
+const ejs = require('ejs');
+const mongoose = require ("mongoose");
 const app = express();
-const port = 3000;
 
-
-app.get('/', (req, res) => {
-  res.send('¡Hola, este es un servidor sencillo con Express!');
-});
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+module.exports = app;
